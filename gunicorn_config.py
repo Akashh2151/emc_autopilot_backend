77f1dff419a8cfe680ed21e99ffe3a792fc39f03
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-08-31 05:57:21 -0700

    cretaeing some changes

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,2 @@
-# gunicorn_config.py
-
-workers = 2      # Number of worker processes
-bind = "0.0.0.0:80"  # Binding address and port
-# timeout = 1       # Maximum request/response timeout
+bind = "0.0.0.0:8080"
+workers = 2
